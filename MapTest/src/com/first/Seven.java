fc0acc07386292f3acfda60ffc5eecfbaa254b5e
SantoshM <malajure.santosh@gmail.com>
2020-05-24 12:04:16 +0530

    Added a New Statement in the File

--- MapTest/src/com/first/Seven.java
+++ MapTest/src/com/first/Seven.java
@@ -1,1 +1,20 @@
 /* Added Seven File */
+package com.first;
+
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Seven {
+
+	public static void main(String[] args) {
+		
+		Map<String,Serializable> mapList = new HashMap<>();
+		mapList.put("companyExits", true);
+		
+		System.out.println(mapList);
+
+	}
+
+}
+
