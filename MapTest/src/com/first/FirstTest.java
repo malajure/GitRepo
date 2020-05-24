a52399003b48660f312a9e04cac065dcee22d381
SantoshM <malajure.santosh@gmail.com>
2020-05-24 11:59:18 +0530

    Modified the changes

--- MapTest/src/com/first/FirstTest.java
+++ MapTest/src/com/first/FirstTest.java
@@ -3,10 +3,9 @@ package com.first;
 public class FirstTest {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
-=======
-	System.out.println("from first line");
->>>>>>> 4dc80f9987788e6979e013f731a1e1665d432c21
+
+		System.out.println("from first line");
+
 		System.out.println("from first line");
 
 	}
